459635cfcb920d111fabb9936aa4704acd7b87ba
InfSub <inf.sub+github.com@gmail.com>
2025-04-27 19:47:22 +0300

    Structure v.0.0.1

---
+++ run_bot.py
@@ -0,0 +1,10 @@
+__author__ = 'InfSub'
+__contact__ = 'https:/t.me/InfSub'
+__copyright__ = 'Copyright (C) 2025, [LegioNTeaM] InfSub'
+__date__ = '2025/04/27'
+__deprecated__ = False
+__maintainer__ = 'InfSub'
+__status__ = 'Development'  # 'Production / Development'
+__version__ = '0.0.1'
+
+from aiogram import Bot, Dispatcher
